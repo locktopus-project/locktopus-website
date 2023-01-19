b0f866f4b92bc3182f9c3748dc9dc5310976b449
cwshkut <alexey.shkut@clockwise.software>
2023-01-19 19:29:07 +0000

    remove trailingSlash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,6 @@ const config = {
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
