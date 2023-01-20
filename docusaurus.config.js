8c2058bff0c384696920dcaeb76c65c1831df884
cwshkut <alexey.shkut@clockwise.software>
2023-01-20 12:47:22 +0000

    use dark theme by default

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,6 +52,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        respectPrefersColorScheme: true
+      },
       announcementBar: {
         id: 'support_ukraine',
         backgroundColor: 'yellow',
