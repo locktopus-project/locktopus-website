9e7b44f2ee00a4e717c150acf2f6df23ccaf02e3
cwshkut <alexey.shkut@clockwise.software>
2023-01-26 13:36:51 +0000

    fix typo

--- .cloud/append_index_html.js
+++ .cloud/append_index_html.js
@@ -1,8 +1,8 @@
 function handler(event, context, callback) {
     var request = event.request
     if (request.method === "GET") {
-        var splitted = request.uri.split('/')
-        var lastSegment = splitted[splitted.length - 1]
+        var split = request.uri.split('/')
+        var lastSegment = split[split.length - 1]
         if (!lastSegment.includes('.')) {
             request.uri = request.uri + (request.uri.endsWith('/') ? '' : '/') + 'index.html';
         }
